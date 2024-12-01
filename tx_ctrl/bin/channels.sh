4b1f7f9c8721ecb740bca5b86885793b6e1e2f6f
snokvist <joakim.snokvist@gmail.com>
2024-12-01 07:11:17 +0100

    Update channels.sh

--- tx_ctrl/bin/channels.sh
+++ tx_ctrl/bin/channels.sh
@@ -9,22 +9,22 @@ then
 	then
 		kill -9 $(pgrep tunnel_rx)
 		#echo -n 'set mode pid' | nc 127.0.0.1 9995
-		tunnel_rx -p 160 -u 5800 -K /etc/drone.key -i 7669206 wlan0 2>&1 | tx_ctrl --wlanid wlan0 --card-type rtl8733bu --alink &
+		tunnel_rx -p 160 -u 5800 -K /etc/drone.key -i 7669206 wlan0 2>&1 | tx_ctrl --wlanid wlan0 --card-type rtl8733bu --alink --verbose --target-value=60 &
 		sleep 1
 		echo  "&L04 &F22 CPU:&C &B temp:&T Starting tx_ctrl ..." > /tmp/MSPOSD.msg
 		
 	elif [ $2 -gt 1150 ] && [ $2 -lt 1250 ]
 	then
-		20mhz-mcs1
+		tx_fallback.sh down
 	elif [ $2 -gt 1350 ] && [ $2 -lt 1450 ]
 	then
-		20mhz-mcs2
+		tx_low_signal.sh down
 	elif [ $2 -gt 1550 ] && [ $2 -lt 1650 ]
 	then
-		40mhz-mcs1
+		tx_high_signal.sh up
 	elif [ $2 -gt 1750 ] && [ $2 -lt 1850 ]
 	then
-		40mhz-mcs2
+		tx_ultra.sh up
 	elif [ $2 -gt 1950 ]
 	then
 		#reset tx_ctrl
@@ -35,9 +35,12 @@ then
 		sleep 0.5
 		/etc/init.d/S95majestic restart
 		sleep 2
-		echo  "&L04 &F22 CPU:&C &B temp:&T Starting tx_ctrl ..." > /tmp/MSPOSD.msg
+		echo setprecrop 0 0 240 0 1440 1080 > /proc/mi_modules/mi_vpe/mi_vpe0
+		sleep 0.5
+		echo  "&L04 &F22 CPU:&C &B temp:&T" > /tmp/MSPOSD.msg
 		sleep 0.2
 		wfb_tx_cmd 8000 set_radio -B20 -G0 -S0 -L1 -M1 -N0
+		
 	fi
 fi
 
